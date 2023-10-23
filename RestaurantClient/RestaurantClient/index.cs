2498e47ed98bd49e8fc7fcc228d6eeffbad3456b
Holger Jüptner - creativITy GmbH <holger.jueptner@creativity-gmbh.de>
2023-10-23 11:17:13 +0200

    added models and test button

--- RestaurantClient/RestaurantClient/index.cs
+++ RestaurantClient/RestaurantClient/index.cs
@@ -9,6 +9,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using Microsoft.VisualBasic;
+using RestaurantClient.Properties;
+using System.Text.Json;
+using Restaurant.Models;
 
 namespace RestaurantClient
 {
@@ -61,6 +64,31 @@ namespace RestaurantClient
             intselectedTable = Int32.Parse(((Button)sender).Text.Remove(0, 6));
         }
 
+        private async void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            //ApiClient apiClient = new ApiClient();
+            //string apiUrl = "https://localhost:1337/orders/7";
+            //string result = await apiClient.GetDataFromApi(apiUrl);
+            //Bestellung bestellung = new Bestellung();
+            //Bestellung bestellung2 = await apiClient.GetDataFromApiGeneric<Bestellung>(apiUrl);
 
+            //if (result != null)
+            //{
+            //    Console.WriteLine("API response:");
+            //    Console.WriteLine(result);
+            //    bestellung = JsonSerializer.Deserialize<Bestellung>(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("API request failed or encountered an error.");
+            //}
+            //Console.WriteLine("");
+
+            ApiClient apiClient = new ApiClient();
+            string apiUrl = "https://localhost:1337/tables";
+            //string result = ...
+            List<Tisch> tische = await apiClient.GetDataFromApiGeneric<List<Tisch>>(apiUrl);
+            Console.WriteLine("");
+        }
     }
 }
